b1b95d43538dfc266c6f3e10afab57987205d398
ntarche <nelson.tarche@gmail.com>
2021-04-01 21:05:14 -0300

    cambios

---
+++ formulario_login/acceso-confirmado.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 my-3">
+                <h1>Bienvenid@ al sistema</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="alert alert-primary" role="alert">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nobis est quod necessitatibus excepturi deserunt tempora odit beatae, possimus error in quidem dolore provident impedit praesentium perspiciatis corrupti sequi architecto!
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <a href="index.php" class="btn btn-primary float-end">Salir</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ formulario_login/index.php
@@ -0,0 +1,54 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+if($_POST){ /* es postback ? */
+
+    $usuario = $_REQUEST["txtUsuario"];
+    $clave = $_REQUEST["txtClave"];
+
+    if($usuario != "" && $clave != ""){
+        header("Location: acceso-confirmado.php");
+    } else {
+        echo "Válido para usuarios registrados";
+        exit;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mt-5">
+                <h1>Formulario</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <form method="POST" action="">
+                    <div class="my-3">
+                        <label for="">Usuario: <input type="text" id="txtUsuario" name="txtUsuario" class="form-control"></label>
+                    </div>
+                    <div class="my-3">
+                        <label for="">Clave: <input type="password" id="txtClave" name="txtClave" class="form-control"></label>
+                    </div>
+                    <div class="my-3">
+                        <button class="btn btn-primary" type="submit">ENVIAR</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
