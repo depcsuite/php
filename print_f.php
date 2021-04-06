b3c7ceeb4ac03e2800d8a09b6b0ab4c3cce3338a
ntarche <nelson.tarche@gmail.com>
2021-04-06 20:46:39 -0300

    cambios

--- print_f.php
+++ print_f.php
@@ -7,15 +7,16 @@ function print_f($variable){
         foreach($variable as $item){
             $contenido .= $item . "\n";
         }
+        file_put_contents("datos.txt", $contenido);
     } else {
-        $contenido = $variable;
+        file_put_contents("datos.txt", $variable);
     }
-    file_put_contents("datos.txt", $contenido);
 }
 
 //Uso
-$aNotas = array(8,5,7,9,10);
-$msg = "Error al enviar el correo electrónico";
+$aNotas = array(8, 5, 7, 9, 10);
+$msg = "Este es un mensaje";
+
 print_f($aNotas);
 print_f($msg);
 
---
+++ print_f1.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Definición
+function print_f($variable){
+	if(is_array($variable)){
+        //Si es un array, lo recorro y guardo el contenido en el archivo “datos.txt”
+           
+	} else {
+  	    //Entonces es string, guardo el contenido en el archivo “datos.txt”
+          file_put_contents("datos.txt", $variable);
+    }
+}
+//Uso
+$aNotas = array(8,5,7,9,10);
+$msg = "Este es un mensaje";
+
+print_f($msg);
+
+?>
