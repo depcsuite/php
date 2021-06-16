2d4c4d39d8e6bea434ff432f5c9df7c20acdb274
ntarche <nelson.tarche@gmail.com>
2021-06-16 19:07:52 -0300

    cambios

---
+++ formulario_invitados.php
@@ -0,0 +1,81 @@
+<?php
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+ini_set('error_reporting', E_ALL);
+
+$aInvitados = array("pepe", "ana", "maca", "juan");
+
+if ($_POST) {
+    if (isset($_REQUEST['btnProcesar'])) {
+        $nombre = $_REQUEST['txtNombre'];
+        if (in_array($nombre, $aInvitados)) {
+            $aMensaje = array("texto" => "¡Bienvenid@ $nombre a la fiesta!", 
+                              "estado" => "success");
+        } else {
+            $aMensaje = array("texto" => "No se encuentra en la lista de invitados.", 
+                              "estado" => "danger");
+        }
+    } else if (isset($_REQUEST['btnVip'])) {
+        $respuesta = $_REQUEST['txtPregunta'];
+        if ($respuesta == "verde") {
+            $aMensaje = array("texto" => "Aquí tiene su pulsera", "estado" => "success");
+
+        } else {
+            $aMensaje = array("texto" => "Ud. no tiene pase VIP", "estado" => "danger");
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900"
+        rel="stylesheet">
+    <title>Listado de invitados</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+	<main class="container">
+    <div class="row">
+        <div class="col-12 py-3">
+            <h1>Listado de ingreso</h1>
+        </div>
+        <?php if(isset($aMensaje)): ?>
+        <div class="col-12">
+            <div class="alert alert-<?php echo $aMensaje["estado"]; ?>" role="alert">
+                <?php echo $aMensaje["texto"]; ?>
+            </div>
+        </div>
+        <?php endif; ?>
+        <div class="col-12">
+            <p>Complete el siguiente formulario:</p>
+        </div>
+        <div class="col-12">
+            <form method="POST" action="">
+                <div class="row">
+                    <div class="col-12">
+                        <p>Nombre:<p><input type="text" name="txtNombre"><br><br>
+                        <input type="submit" name="btnProcesar" value="Procesar invitado" class="btn-primary">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="12-col bm-3">
+                        <p>Ingresa el código secreto para el pase VIP:<p><input type="text" name="txtPregunta"><br><br>
+                        <input type="submit" name="btnVip" value="Procesar VIP" class="btn-primary">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+	</main>
+</body>
+</html>
--- prueba.php
+++ prueba.php
@@ -1,69 +1,17 @@
-
 <?php
 
-if ($_POST) {
-    if (isset($_REQUEST['btnProcesar'])) {
-        $nombre = $_REQUEST['nombre'];
-        if ($nombre == "maca") {
-            $mensaje = "$nombre ,bienvenida a la fiesta formidable.";
-        } else if ($nombre == "cande") {
-            $mensaje = "$nombre ,adelante puede pasar";
-        } else if ($nombre == "nico") {
-            $mensaje = "$nombre ,pase usted que es el organizador de la fiesta don";
-        } else {
-			$mensaje = "Usted no se encuentra en la lista.";
-		}
-    } else if(isset($_REQUEST['btnPregunta'])) {
-        $pregunta = $_REQUEST['pregunta'];
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+ini_set('error_reporting', E_ALL);
 
-        if ($pregunta == "verde") {
-            $respuesta = " aqui tiene su pulsera";
-        } else {
-            $respuesta = " usted no es invitado premium";
-        }
-    }
+function retornarValorPositivo(float $numero): ?int {
+    //return $numero > 0 ? $numero : null;
+    return $numero + 1.50;
+
 }
+
+//echo retornarValorPositivo(5);
+//echo retornarValorPositivo(-5);
+echo retornarValorPositivo(5.80);
+
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
-    <link rel="stylesheet" href="css/estilos.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900"
-        rel="stylesheet">
-    <title>isset</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-</head>
-<body>
-	<main class="container">
-        <div class="row">
-            <div class="12-col bm-3">
-					<h1>La fiesta</h1>
-					<p>Complete el siguiente formularo:</p>
-            </div>
-			<div class="col-12">
-				<form method="POST" action="">
-					<p>Nombre:<p><input type="text" name="nombre"><br><br>
-					<input type="submit" name="btnProcesar" value="Procesar" class="btn-primary">
-					<?php echo isset($mensaje) ? $mensaje : ""; ?>
-				</form>
-			</div>
-		</div>
-		<div class="row">
-			<div class="12-col bm-3">
-				<form method="POST" action="">
-					<p>Pregunta: tenes el pase premium?<p><input type="text" name="pregunta"><br><br>
-					<input type="submit" name="btnPregunta" value="Procesar" class="btn-primary">
-					<?php echo isset($respuesta) ? $respuesta : ""; ?>
-				</form>
-			</div>
-		</div>
-	</main>
-</body>
-</html>
