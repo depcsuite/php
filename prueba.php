ce2f0e47fdf82d02015adf7297236bee2cec46bd
ntarche <nelson.tarche@gmail.com>
2021-07-01 19:54:20 -0300

    cambios

--- prueba.php
+++ prueba.php
@@ -1,50 +1,55 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio</title>
+    <title>Artículos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <script>
- 
-    aNumeros = new Array(10, 14, 21, 42, 7, 35, 42, 100);
+    let aProductos = new Array(
+        ["Yerba", 200],
+        ["Azúcar", 50],
+        ["Tallarines", 80],
+        ["Leche", 40]
+    );
 
-    function imprimirListado(array){
-        array.forEach(function(numero){
-            document.write(numero + "<br>");
-        })
+    function actualizarPrecios(aValores, porcentaje){
+        return aValores.map(function(numero) {
+            return new Array(numero[0], numero[1] * ((porcentaje/100)+1));
+        });
+
     }
 
-    document.write("Con foreach, los múltiplos de 7 son :<br>");
-    aNumeros.forEach(function (value, index) {
-        if(value % 7 == 0){
-            document.write(value + "<br>");
-        }
-    });
-
-    document.write("Listado total de números:<br>");
-    imprimirListado(aNumeros);
-
-    document.write("Con filter, los múltiplos de 7 son:<br>");
-    aMultiplos = aNumeros.filter(function (value) {
-        return (value % 7 == 0);
-    });
-    imprimirListado(aMultiplos);
-
-
-    document.write("Función map:<br>");
-    aRaiz = aNumeros.map(function (value) {
-        return Math.sqrt(value);
-    });
-    imprimirListado(aRaiz);
-
+    function imprimirPoductos(aArticulos) {
+        document.write(`<main class="container">
+        <div class="row">
+            <div class="col-12 py-5">
+                <h1>Lista actualizada de precios</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-hover border">
+                    <tr><th>Producto</th><th>Precio</th></tr>`);
+                aArticulos.forEach(function(value, index) {
+                    document.write(`<tr><td>${value[0]}</td><td>${value[1].toFixed(2)}</td></tr>`);
+                });
    
+           document.write(`
+                </table>
+            </div>
+        </div>
+    </main>`);
+    }
+
+    //Programa
+     aProductos = actualizarPrecios(aProductos, 10);
+    imprimirPoductos(aProductos);
+
 </script>
-
-<body></body>
-
+<body>
+    
 </body>
-
 </html>
