d54858ef34b81c572dde0a3445066f2235be6b07
ntarche <nelson.tarche@gmail.com>
2021-07-21 19:27:23 -0300

    cambios

--- login_resuelto/entidades/localidad.php
+++ login_resuelto/entidades/localidad.php
@@ -18,22 +18,10 @@ class Localidad{
         $this->$atributo = $valor;
         return $this;
     }
-
-
-
-
-
-
-
-
-
-
-
-
     
     public function obtenerPorProvincia($idProvincia){
         $aLocalidades = null;
-        $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE);
+        $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
         $sql = "SELECT 
             idlocalidad,
             nombre, 
