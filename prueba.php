4cfa2bb005f64caa4cf9ed2576ef8acd8b090fc1
ntarche <nelson.tarche@gmail.com>
2021-09-08 20:54:52 -0300

    cambios

--- prueba.php
+++ prueba.php
@@ -1,49 +1,87 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
+//Definicion de clases
 
 class Persona{
+    protected $dni;
+    protected $nombre;
+    protected $correo;
+    protected $celular;
 
-    private $dni;
+    public function __construct() {
+
+    }
+
+    public function __get($propiedad) {
+        return $this->$propiedad;
+    }
+
+    public function __set($propiedad, $valor) {
+        $this->$propiedad = $valor;
+    }
+}
+
+class Alumno extends Persona {
+    private $fechaNac;
+    private $peso;
+    private $altura;
+    private $aptoFisico;
+    private $presentismo;
+
+    public function __construct($dni, $nombre, $correo, $telefono, $fechaNac) {
+        $this->dni = $dni;
+        $this->nombre = $nombre;
+        $this->correo = $correo;
+        $this->telefono = $telefono;
+        $this->fechaNac = $fechaNac;
+        $this->peso = 0.0;
+        $this->altura = 0.0;
+        $this->presentismo = 0.0;
+        $this->aptoFisico = false;
+    }
+
+    public function __get($propiedad) {
+        return $this->$propiedad;
+    }
+
+    public function __set($propiedad, $valor) {
+        $this->$propiedad = $valor;
+    }
+
+    public function setFichaMedica($peso, $altura, $aptoFisico){
+        $this->peso = $peso;
+        $this->altura = $altura;
+        $this->aptoFisico = $aptoFisico;
+    }
+}
+
+class Entrenador extends Persona{
+    private $aClases;
+
+    public function __construct($dni, $nombre, $correo, $telefono) {
+        $this->dni = $dni;
+        $this->nombre = $nombre;
+        $this->correo = $correo;
+        $this->telefono = $telefono;
+        $this->aClases = array();
+    }
+
+    public function __get($propiedad) {
+        return $this->$propiedad;
+    }
+
+    public function __set($propiedad, $valor) {
+        $this->$propiedad = $valor;
+    }
+}
+
+class Clase{
     private $nombre;
-    private $edad;
-    private $nacionalidad;
+    private $entrenador;
+    private $aAlumnos;
 
-    public function getDni(){ return $this->dni; }
-    public function setDni($dni){ $this->dni = $dni; }
-
-    public function getNombre(){ return $this->nombre; }
-    public function setNombre($nombre){ $this->nombre = $nombre; }
-
-    public function getEdad(){ return $this->edad; }
-    public function setEdad($edad){ $this->edad = $edad; }
-   
-    public function getNacionalidad(){ return $this->nacionalidad; }
-    public function setNacionalidad($nacionalidad){ $this->nacionalidad = $nacionalidad; }
- 
-
-    public function imprimir(){
-        echo "DNI: $this->dni<br>";
-        echo "Nombre: $this->nombre<br>";
-        echo "Edad: $this->edad<br>";
-        echo "Nacionalidad: $this->nacionalidad<br><br>";
-    }
-
-    public function __destruct() {
-      echo "Destruyendo el objeto " . $this->nombre . "<br>";
-    }
-
-}
-
-class Alumno extends Persona
-{
-    private $legajo;
-
-    public function __construct(){
-        $this->notaPortfolio = 0.0;
-        $this->notaPHP = 0.0;
-        $this->notaProyecto = 0.0;
+    public function __construct() {
+        $this->aAlumnos = array();
     }
 
     public function __get($propiedad) {
@@ -54,93 +92,56 @@ class Alumno extends Persona
         $this->$propiedad = $valor;
     }
 
-    public function imprimir(){
-        echo "DNI: " . $this->dni . "<br>";
-        echo "Nombre: " . $this->nombre . "<br>";
-        echo "Edad: " . $this->edad . "<br>";
-        echo "Nacionalidad: " . $this->nacionalidad . "<br>";
-        echo "Legajo:" . $this->legajo . "<br>";
-        echo "Nombre: " . $this->nombre . "<br>";
-        echo "Nota Portfolio: " . $this->notaPortfolio . "<br>";
-        echo "Nota PHP: " . $this->notaPHP . "<br>";
-        echo "Nota Proyecto: " . $this->notaProyecto . "<br>";
-        echo "Promedio: ". number_format($this->calcularPromedio(), 2, ',', '.') . "<br><br>";
+    public function inscribirAlumno($alumno){
+        $this->aAlumnos[] = $alumno;
     }
 
-    public function calcularPromedio(){
-        $promedio = ($this->notaPortfolio + $this->notaPHP + $this->notaProyecto) / 3;
-        return $promedio;
+    public function asignarEntrenador($entrenador){
+        $this->entrenador = $entrenador;
     }
 
-    public function __destruct() {
-      echo "Destruyendo el objeto " . $this->nombre . "<br>";
+    public function imprimirListado(){
+
     }
+
 }
 
-class Docente extends Persona{
-    private $especialidad;
-    const ESPECIALIDAD_WP = "Wordpress";
-    const ESPECIALIDAD_ECO = "Economía aplicada";
-    const ESPECIALIDAD_BBDD = "Base de datos";
-
-    public function __get($propiedad) {
-        return $this->$propiedad;
-    }
-
-    public function __set($propiedad, $valor) {
-        $this->$propiedad = $valor;
-    }
-
-    public function imprimir(){
-        echo "DNI: " . $this->dni . "<br>";
-        echo "Nombre: " . $this->nombre . "<br>";
-        echo "Edad: " . $this->edad . "<br>";
-        echo "Nacionalidad: " . $this->nacionalidad . "<br>";
-        echo "Especialidad: " . $this->especialidad . "<br><br>";
-    }
-    public function imprimirEspecialidadesHabilitadas(){
-        echo "Las especialidades habilitadas son:<br>";
-        echo self::ESPECIALIDAD_BBDD . "<br>";
-        echo self::ESPECIALIDAD_ECO . "<br>";
-        echo self::ESPECIALIDAD_WP . "<br>";
-    }
-
-    public function __destruct() {
-      echo "Destruyendo el objeto " . $this->nombre . "<br>";
-    }
-}
 
 //Programa
-$persona1 = new Persona();
-$persona1->setDni("35678321");
-echo $persona1->getDni();
-$persona1->setNombre("Juan Perez");
-$persona1->setEdad(30);
-$persona1->setNacionalidad("Argentina");
-$persona1->imprimir();
+$entrenador1 = new Entrenador("34987789", "Miguel Ocampo", "miguel@mail.com", "11678634");
+$entrenador2 = new Entrenador("28987589", "Andrea Zarate", "andrea@mail.com", "11768654");
 
-$alumno1 = new Alumno();
-$alumno1->nombre = "Julia Lopez";
-$alumno1->dni = "38979977";
-$alumno1->nacionalidad = "Argentina";
-$alumno1->legajo = 7898;
-$alumno1->notaPhp = 8.5;
-$alumno1->notaPortfolio = 7.5;
-$alumno1->notaProyecto = 8;
-$alumno1->imprimir();
+$alumno1 = new Alumno("40787657", "Dante Montera", "dante@mail.com", "1145632457", "1997-08-28");
+$alumno1->setFichaMedica(90, 178, true);
+$alumno1->presentismo = 78;
 
-$alumno2 = new Alumno();
-$alumno2->nombre = "Matías Diaz";
-$alumno2->nacionalidad = "Colombiano";
-$alumno2->notaPortfolio = 9;
-$alumno2->notaPhp = 9;
-$alumno2->notaProyecto = 8;
-$alumno2->imprimir();
+$alumno2 = new Alumno("46766547", "Darío Turchi", "dante@mail.com", "1145632457", "1986-11-21");
+$alumno2->setFichaMedica(73, 168, false);
+$alumno2->presentismo = 68;
 
-$docente1 = new Docente();
-$docente1->nombre = "Juan Carlos Rosales";
-$docente1->especialidad = Docente::ESPECIALIDAD_ECO;
-$docente1->imprimir();
+$alumno3 = new Alumno("39765454", "Facundo Fagnano", "facundo@mail.com", "1145632457", "1993-02-06");
+$alumno3->setFichaMedica(90, 187, true);
+$alumno3->presentismo = 88;
+
+$alumno4 = new Alumno("41687536", "Gastón Aguilar", "gaston@mail.com", "1145632457", "1999-11-02");
+$alumno4->setFichaMedica(70, 169, false);
+$alumno4->presentismo = 98;
+
+$clase1 = new Clase();
+$clase1->nombre = "Funcional";
+$clase1->asignarEntrenador($entrenador1);
+$clase1->inscribirAlumno($alumno1);
+$clase1->inscribirAlumno($alumno3);
+$clase1->inscribirAlumno($alumno4);
+$clase1->imprimirListado();
+
+$clase2 = new Clase();
+$clase2->nombre = "Zumba";
+$clase2->asignarEntrenador($entrenador2);
+$clase2->inscribirAlumno($alumno1);
+$clase2->inscribirAlumno($alumno2);
+$clase2->inscribirAlumno($alumno3);
+$clase1->imprimirListado();
 
 
 ?>
