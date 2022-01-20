4e6231e256f0705557c28356bb4ae69f76a12055
ntarche <nelson.tarche@gmail.com>
2022-01-20 12:30:20 -0300

    cambios

--- funcion_promediar.php
+++ funcion_promediar.php
@@ -1,7 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 function promediar($aNumeros){
     $suma = 0;
+    $resultado = 0;
     foreach($aNumeros as $numero){
         $suma += $numero;
     }
