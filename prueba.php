89abfcf905a16347f412e361e67e369dc0e04d56
ntarche <nelson.tarche@gmail.com>
2022-05-24 19:02:05 -0300

    cambios

--- prueba.php
+++ prueba.php
@@ -3,6 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$claveEncriptada = password_hash("admin123", PASSWORD_DEFAULT);
 
-echo $claveEncriptada;
+$cadena = "33300012,33300015,33300016,33300017,33300018,33300019,33300020";
+$miArray = explode(",", $cadena);
+
+print_r($miArray);
+
+
+?>
+
--- sistema_ventas/header.php
+++ sistema_ventas/header.php
@@ -1,16 +1,5 @@
 <?php
-session_start();
 
-if(!isset($_SESSION["nombre"])){
-    header("Location: login.php");
-}
-
-if($_POST){
-    if(isset($_POST["btnCerrar"])){ /* Analizamos si es la accion del boton cerrar */
-        session_destroy();
-        header("location:login.php");
-    }
-}
 ?>
 
 <!DOCTYPE html>
