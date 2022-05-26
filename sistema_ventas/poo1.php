ea5b392c22c22580d980d58d16e6cb6c92f0fd53
ntarche <nelson.tarche@gmail.com>
2022-05-26 10:35:43 -0300

    cambios

---
+++ sistema_ventas/poo1.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class Persona{
+
+
+
+}
+
+class Alumno extends Persona{
+
+    
+}
+
+class Docente extends Persona{
+
+
+
+}
+
+?>
