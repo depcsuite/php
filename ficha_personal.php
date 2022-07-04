c6274458fb1f7d02fddcf90e49f3a26200c5b236
ntarche <nelson.tarche@gmail.com>
2022-07-04 20:07:08 -0300

    cambios

--- ficha_personal.php
+++ ficha_personal.php
@@ -1,14 +1,13 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
+date_default_timezone_set("America/Argentina/Buenos_Aires");
+//date_default_timezone_set("America/Bogota");
 
-$fecha = date("d/m/Y");
 $nombre = "Nelson Daniel Tarche";
-$edad = 33;
-$aPeliculas = array("Volver al futuro", "Titanic", "El despues de mañana");
+$edad = 34;
+//$aPeliculas = array("Volver al futuro", "Titanic", "El día después de mañana");
 
+$aPeliculas = ["Volver al futuro", "Titanic", "El día después de mañana"]; //Otra forma
 
 ?>
 <!DOCTYPE html>
@@ -18,7 +17,8 @@ $aPeliculas = array("Volver al futuro", "Titanic", "El despues de mañana");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ficha personal</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
     <main class="container">
@@ -30,30 +30,29 @@ $aPeliculas = array("Volver al futuro", "Titanic", "El despues de mañana");
         <div class="row">
             <div class="col-12">
                 <table class="table table-hover border">
-                    <tbody>
-                        <tr>
-                            <th>Fecha:</th>
-                            <td><?php echo $fecha; ?></td>
-                        </tr>
-                        <tr>
-                            <th>Nombre y apellido:</th>
-                            <td><?php echo $nombre; ?></td>
-                        </tr>
-                        <tr>
-                            <th>Edad:</th>
-                            <td><?php echo $edad; ?></td>
-                        </tr>
-                        <tr>
-                            <th>Películas favoritas</th>
-                            <td><?php echo $aPeliculas[0]; ?><br>
-                                <?php echo $aPeliculas[1]; ?><br>
-                                <?php echo $aPeliculas[2]; ?>
-                            </td>
-                        </tr>
-                    </tbody>
+                    <tr>
+                        <th>Fecha:</th>
+                        <td><?php echo date("d/m/Y"); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Nombre y apellido:</th>
+                        <td><?php echo $nombre; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Edad:</th>
+                        <td><?php echo $edad; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Películas favoritas</th>
+                        <td><?php echo $aPeliculas[0]; ?><br>
+                            <?php echo $aPeliculas[1]; ?><br>
+                            <?php echo $aPeliculas[2]; ?>
+                        </td>
+                    </tr>
                 </table>
             </div>
         </div>
+
     </main>
 </body>
 </html>
--- prueba.php
+++ prueba.php
@@ -1,5 +1,19 @@
 <?php
 
-echo "Test";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$aAuto = array();
+$aAuto["color"] = array("Negro", "Verde");
+$aAuto["marca"] = "Ford";
+$aAuto["anio"] = 1908;
+$aAuto["precio"] = 800;
+
+echo $aAuto["color"][0];
+print_r($aAuto);
+
+
 
 ?>
