1b92d6761433a965e50e3d8ece3f1c4c4a902606
ntarche <nelson.tarche@gmail.com>
2022-11-08 21:20:52 -0300

    cambios

--- prueba.php
+++ prueba.php
@@ -4,4 +4,143 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo password_hash("admin123", PASSWORD_DEFAULT);
+class Persona
+{
+    protected $documento;
+    protected $nombre;
+    protected $correo;
+    protected $celular;
+
+    public function __get($propiedad)
+    {
+        return $this->$propiedad;
+    }
+
+    public function __set($propiedad, $valor)
+    {
+        $this->$propiedad = $valor;
+    }
+}
+
+class Alumno extends Persona
+{
+    private $fecNacimiento;
+    private $peso;
+    private $altura;
+    private $aptoFisico;
+    private $presentismo;
+
+    public function __construct($dni, $nombre, $correo, $celular)
+    {
+        $this->dni = $dni;
+        $this->nombre = $nombre;
+        $this->correo = $correo;
+        $this->celular = $celular;
+        $this->peso = 0.0;
+        $this->altura = 0.0;
+        $this->aptoFisico = false;
+        $this->presentismo = 0.0;
+    }
+
+    public function __get($propiedad)
+    {
+        return $this->$propiedad;
+    }
+
+    public function __set($propiedad, $valor)
+    {
+        $this->$propiedad = $valor;
+    }
+
+    public function setFichaMedica($peso, $altura, $aptoFisico) {
+        $this->peso = $peso;
+        $this->altura = $altura;
+        $this->aptoFisico = $aptoFisico;
+    }
+}
+
+class Entrenador extends Persona
+{
+    private $aClases;
+
+    public function __construct($documento, $nombre, $correo, $telefono)
+    {
+        $this->documento = $documento;
+        $this->nombre = $nombre;
+        $this->correo = $correo;
+        $this->telefono = $telefono;
+        $this->aClases = array();
+    }
+
+    public function __get($propiedad)
+    {
+        return $this->$propiedad;
+    }
+
+    public function __set($propiedad, $valor)
+    {
+        $this->$propiedad = $valor;
+    }
+
+    public function asignarClase()
+    {
+
+    }
+}
+
+class Clase
+{
+    private $nombre;
+    private $entrenador;
+    private $aAlumnos;
+
+    public function __construct()
+    {
+        $this->aAlumnos = array();
+    }
+
+    public function __get($propiedad)
+    {
+        return $this->$propiedad;
+    }
+
+    public function __set($propiedad, $valor)
+    {
+        $this->$propiedad = $valor;
+    }
+
+    public function asignarEntrenador()
+    {
+
+    }
+
+    public function inscribirAlumnos()
+    {
+
+    }
+
+    public function imprimirListado()
+    {
+
+    }
+}
+
+//Desarrollo del programa
+$entrenador1 = new Entrenador("34987789", "Miguel Ocampo", "miguel@mail.com", "11678634");
+$entrenador2 = new Entrenador("28987589", "Andrea Zarate", "andrea@mail.com", "11768654");
+
+$alumno1 = new Alumno("40787657", "Dante Montera", "dante@mail.com", "1145632457", "1997-08-28");
+$alumno1->setFichaMedica(90, 178, true);
+$alumno1->presentismo = 78;
+
+$alumno2 = new Alumno("46766547", "Darío Turchi", "dante@mail.com", "1145632457", "1986-11-21");
+$alumno2->setFichaMedica(73, 168, false);
+$alumno2->presentismo = 68;
+
+$alumno3 = new Alumno("39765454", "Facundo Fagnano", "facundo@mail.com", "1145632457", "1993-02-06");
+$alumno3->setFichaMedica(90, 187, true);
+$alumno3->presentismo = 88;
+
+$alumno4 = new Alumno("41687536", "Gastón Aguilar", "gaston@mail.com", "1145632457", "1999-11-02");
+$alumno4->setFichaMedica(70, 169, false);
+$alumno4->presentismo = 98;
