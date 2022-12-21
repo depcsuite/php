4eb1817bf47e96cde6637aaad810a3c5cd3cbce6
ntarche <nelson.tarche@gmail.com>
2022-12-21 20:38:54 -0300

    cambios

--- abmclientes1/index.php
+++ abmclientes1/index.php
@@ -38,8 +38,8 @@ if($_POST){
         }
     }
 
-    if(isset($_GET["pos"]) && $_GET["pos"] >= 0){
-        $pos = $_GET["pos"];
+    if(isset($_GET["editar"]) && $_GET["editar"] >= 0){
+        $pos = $_GET["editar"];
 
         if($_FILES["archivo"]["error"] !== UPLOAD_ERR_OK){
             //Si no se adjunta una imagen, recuperar el nombre de la imagen del cliente almacenado
@@ -75,8 +75,8 @@ if($_POST){
     file_put_contents("archivo.txt", $jsonClientes);
 }
 
-if(isset($_GET["pos"]) && $_GET["pos"] >= 0){
-    $pos = $_GET["pos"];
+if(isset($_GET["editar"]) && $_GET["editar"] >= 0){
+    $pos = $_GET["editar"];
 }
 
 if(isset($_GET["eliminar"]) && $_GET["eliminar"] >= 0){
@@ -111,8 +111,7 @@ if(isset($_GET["eliminar"]) && $_GET["eliminar"] >= 0){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ABM Clientes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 </head>
 <body>
     <main class="container">
@@ -174,8 +173,8 @@ if(isset($_GET["eliminar"]) && $_GET["eliminar"] >= 0){
                                 <td><?php echo $cliente["nombre"]; ?></td>
                                 <td><?php echo $cliente["correo"]; ?></td>
                                 <td>
-                                    <a href="?pos=<?php echo $pos;?>"><i class="fa-solid fa-pen-to-square"></i></a>
-                                    <a href="?eliminar=<?php echo $pos;?>"><i class="fa-solid fa-trash-can"></i></a>
+                                    <a href="?editar=<?php echo $pos; ?>"><i class="bi bi-zoom-in"></i></a>
+                                    <a href="?eliminar=<?php echo $pos; ?>"><i class="bi bi-trash3"></i></a>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
