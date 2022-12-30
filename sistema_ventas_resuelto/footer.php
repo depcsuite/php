edc2c2b564041cb0104f50cbe5e3fec549fc90a4
ntarche <nelson.tarche@gmail.com>
2022-12-30 11:16:28 -0300

    cambios

--- sistema_ventas_resuelto/footer.php
+++ sistema_ventas_resuelto/footer.php
@@ -1,1 +1,47 @@
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span><a href="https://depcsuite.com" target="_blank">Patrocinado por DePC Suite</a></span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
   </form>
+  <!-- Logout Modal-->
+  <form action="" method="POST">
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Desea salir del sistema?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Hacer clic en "Cerrar sesión" si deseas finalizar tu sesión actual.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+            <button type="submit" class="btn btn-primary" name="btnCerrar">Cerrar sesión</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
+</body>
+
+</html>
