37875e396c56214e94640096637c05acb0040e39
ntarche <nelson.tarche@gmail.com>
2023-05-30 18:49:14 -0300

    cambios

--- socios.php
+++ socios.php
@@ -26,7 +26,7 @@ abstract class Persona
     abstract public function imprimir();
 }
 
-class Cliente extends Persona
+class Socio extends Persona
 {
     private $aTarjetas;
     private $bActivo;
@@ -102,25 +102,25 @@ class Tarjeta
 }
 
 //Desarrollo del programa
-$cliente1 = new Cliente();
-$cliente1->dni = "35123789";
-$cliente1->nombre = "Ana Valle";
-$cliente1->correo = "ana@correo.com";
-$cliente1->celular = "1156781234";
+$socio1 = new Socio();
+$socio1->dni = "35123789";
+$socio1->nombre = "Ana Valle";
+$socio1->correo = "ana@correo.com";
+$socio1->celular = "1156781234";
 $tarjeta1 = new Tarjeta(Tarjeta::VISA, "4223750778806383", "06/2020", "01/2023", "275");
 $tarjeta2 = new Tarjeta(Tarjeta::AMEX, "347572886751981", "01/2022", "07/2027", "136");
 $tarjeta3 = new Tarjeta(Tarjeta::MASTERCARD, "5415620495970009", "07/2021", "12/2024", "742");
-$cliente1->agregarTarjeta($tarjeta1);
-$cliente1->agregarTarjeta($tarjeta2);
-$cliente1->agregarTarjeta($tarjeta3);
+$socio1->agregarTarjeta($tarjeta1);
+$socio1->agregarTarjeta($tarjeta2);
+$socio1->agregarTarjeta($tarjeta3);
 
-$cliente2 = new Cliente();
-$cliente2->dni = "48456876";
-$cliente2->nombre = "Bernabe Paz";
-$cliente2->correo = "bernabe@correo.com";
-$cliente2->celular = "1145326787";
-$cliente2->agregarTarjeta(new Tarjeta(Tarjeta::VISA, "4969508071710316", "06/2021", "08/2025", "865"));
-$cliente2->agregarTarjeta(new Tarjeta(Tarjeta::MASTERCARD, "5149107669552238", "03/2022", "04/2025", "554"));
-$cliente2->darDeBaja(Date("23/8/2022"));
-print_r($cliente1);
-print_r($cliente2);
+$socio2 = new Socio();
+$socio2->dni = "48456876";
+$socio2->nombre = "Bernabe Paz";
+$socio2->correo = "bernabe@correo.com";
+$socio2->celular = "1145326787";
+$socio2->agregarTarjeta(new Tarjeta(Tarjeta::VISA, "4969508071710316", "06/2021", "08/2025", "865"));
+$socio2->agregarTarjeta(new Tarjeta(Tarjeta::MASTERCARD, "5149107669552238", "03/2022", "04/2025", "554"));
+$socio2->darDeBaja(Date("31/05/2023"));
+//print_r($socio1);
+//print_r($socio2);
